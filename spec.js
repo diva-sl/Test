0bf4844aedba66f0b09f26381c54f9bdb6778e1f
sidtharthan <sidthunagarajan@gmail.com>
2019-08-29 10:01:02 +0530

    Add new specs

--- spec.js
+++ spec.js
@@ -34,3 +34,26 @@ const output2 = fn100(input1, a => a.age);
 test(output2.length, 2, 'fn100');
 test(output2[0], 23, 'fn100');
 test(output2[1], 34, 'fn100');
+
+const asc = (a,b) => {
+  return (a<b)? -1: (b<a)? 1: 0;
+};
+const desc = (a,b) => {
+  return (a<b)? 1: (b<a)? -1: 0;
+};
+
+const output4 = fn200([1,4,5,2,3], asc);
+test(output4.length, 5, 'fn200');
+test(output4[0], 1, 'fn200');
+test(output4[1], 2, 'fn200');
+test(output4[2], 3, 'fn200');
+test(output4[3], 4, 'fn200');
+test(output4[4], 5, 'fn200');
+
+const output5 = fn200([1,4,5,2,3], desc);
+test(output5.length, 5, 'fn200');
+test(output5[0], 5, 'fn200');
+test(output5[1], 4, 'fn200');
+test(output5[2], 3, 'fn200');
+test(output5[3], 2, 'fn200');
+test(output5[4], 1, 'fn200');
